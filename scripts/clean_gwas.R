bf95cea225b57db597276222a24b87907dd14d97
hazalkarabulak <hazalkarabulak355@gmail.com>
2026-04-29 22:13:10 +0300

    ET GWAS temizleme scripti

--- scripts/clean_gwas.R
+++ scripts/clean_gwas.R
@@ -1,8 +1,28 @@
 library(data.table)
-dt <- fread("data/raw/G250_Essential_tremor_summary.txt", 
-            select = c(4, 8, 7, 17, 25, 26, 27), 
-            col.names = c("SNP", "A1", "A2", "freq", "OR", "se", "p"))
-dt <- dt[SNP != "." & !is.na(SNP) & OR > 0 & se > 0 & p > 0]
+
+dt <- fread("G250_Essential_tremor_summary.txt", na.strings = c("NA", "NaN", ".", ""))
+cat("Ham satır sayısı:", nrow(dt), "\n")
+
+setnames(dt,
+  old = c("rsID", "EA", "OA", "OR", "SE", "P", "UK_FREQ"),
+  new = c("SNP",  "A1", "A2", "OR", "SE", "p", "freq")
+)
+
+valid_allele <- function(x) grepl("^[ACTGactg]+$", x)
+dt <- dt[valid_allele(A1) & valid_allele(A2)]
+dt <- dt[!is.na(OR) & !is.na(SE) & !is.na(p) & !is.na(freq)]
+dt <- dt[OR > 0 & SE > 0 & p > 0 & p <= 1]
+dt <- dt[freq > 0 & freq < 1]
+
 dt[, b := log(OR)]
+dt[, z := b / SE]
+dt <- dt[abs(z) < 10]
+
+setorder(dt, p)
+dt <- dt[!duplicated(SNP)]
+
 dt[, n := 64614]
-fwrite(dt[, .(SNP, A1, A2, freq, b, se, p, n)], "data/clean/ET_gwas_clean.ma", sep = "\t", quote = FALSE)
+out <- dt[, .(SNP, A1, A2, freq, b, se = SE, p, n)]
+
+fwrite(out, "ET_gwas_clean.ma", sep = "\t", quote = FALSE)
+cat("Temiz satır sayısı:", nrow(out), "\n")
